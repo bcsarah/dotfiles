9d7839c69fb0aaa3bee89fb2d006a5d1fde2da09
Sarah Aurora <sarahalencar6409@gmail.com>
2026-02-28 23:42:26 -0300

    lazygit, ashen adicionados. install.sh atualizado

--- install.sh
+++ install.sh
@@ -16,18 +16,31 @@ cp -r "$CONFIG"/fastfetch "$BACKUP"
 cp -r "$CONFIG"/hypr "$BACKUP"
 cp -r "$CONFIG"/waybar "$BACKUP"
 cp -r "$CONFIG"/wofi "$BACKUP"
+cp -r "$CONFIG"/yazi "$BACKUP"
+cp -r "$CONFIG"/lazygit "$BACKUP"
 
 # System Recreation
 sudo cp "$NIXOS"/hardware-configuration.nix "$DOTFILES"/nixos/
 sudo ln -sfnv "$DOTFILES"/nixos "$NIXOS"
 sudo nixos-rebuild boot
 
+# Removing .config
+rm -rf "$DOTFILES"/nvim
+rm -rf "$DOTFILES"/fastfetch
+rm -rf "$DOTFILES"/hypr
+rm -rf "$DOTFILES"/waybar
+rm -rf "$DOTFILES"/wofi
+rm -rf "$DOTFILES"/yazi
+rm -rf "$DOTFILES"/lazygit
+
 # Symlinks
-ln -sfnv "$DOTFILES"/nvim "$CONFIG"/nvim
-ln -sfnv "$DOTFILES"/fastfetch "$CONFIG"/fastfetch
-ln -sfnv "$DOTFILES"/hypr "$CONFIG"/hypr
-ln -sfnv "$DOTFILES"/waybar "$CONFIG"/waybar
-ln -sfnv "$DOTFILES"/wofi "$CONFIG"/wofi
+ln -sfnv "$DOTFILES"/nvim "$CONFIG"
+ln -sfnv "$DOTFILES"/fastfetch "$CONFIG"
+ln -sfnv "$DOTFILES"/hypr "$CONFIG"
+ln -sfnv "$DOTFILES"/waybar "$CONFIG"
+ln -sfnv "$DOTFILES"/wofi "$CONFIG"
+ln -sfnv "$DOTFILES"/yazi "$CONFIG"
+ln -sfnv "$DOTFILES"/lazygit "$CONFIG"
 
 # Reboot
 reboot
---
+++ lazygit/README.md
@@ -0,0 +1,5 @@
+# LazyGit
+
+To install the theme, kindly add the contents of
+[config.yml](https://codeberg.org/ficd/ashen/raw/branch/main/lazygit/config.yml) to your
+LazyGit configuration, typically located at `~/.config/lazygit/config.yml`.
