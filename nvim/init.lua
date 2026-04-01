207f97ad60bc6674714b2faa387ea01c1bfef0d3
Better Call Sarah <sarahalencar6409@gmail.com>
2026-04-01 07:39:10 -0300

    btop adicionado

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,8 +5,8 @@ require("config.lazy")
 vim.opt.clipboard = "unnamedplus"
 
 -- Indentação
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
